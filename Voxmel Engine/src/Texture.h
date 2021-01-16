f0d666ec220cbb2f4130abbc8770a973b9ea0919
David Kelley <dk1ofakind@gmail.com>
2021-01-16 07:42:54 -0800

    Added stb_image to the project.

---
+++ Voxmel Engine/src/Texture.h
@@ -0,0 +1,17 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <glad/glad.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+class Texture
+{
+public:
+
+private:
+
+};
+
+#endif // !TEXTURE_H
+
