a820ebc53f88d0113ae0a413fa5cb23908e83bab
MrOutsider <dk1ofakind@gmail.com>
2021-01-15 22:29:52 -0800

    Removed some comments

--- Voxmel Engine/src/main.cpp
+++ Voxmel Engine/src/main.cpp
@@ -13,24 +13,6 @@ void processInput(GLFWwindow* window);
 const uint32_t WINDOW_WIDTH = 800;
 const uint32_t WINDOW_HEIGHT = 600;
 
-//const char* vertexShaderSource = "#version 330 core\n"
-//	"layout (location = 0) in vec3 aPos;\n"
-//	"layout (location = 1) in vec3 aColor;\n"
-//	"out vec3 ourColor;\n"
-//	"void main()\n"
-//	"{\n"
-//	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-//	"   ourColor = aColor;\n"
-//	"}\0";
-//
-//const char* fragmentShaderSource = "#version 330 core\n"
-//	"out vec4 FragColor;\n"
-//	"in vec3 ourColor;\n"
-//	"void main()\n"
-//	"{\n"
-//	"	FragColor = vec4(ourColor, 1.0);\n"
-//	"}\0";
-
 int main()
 {
 	// GLFW : Init and config window
