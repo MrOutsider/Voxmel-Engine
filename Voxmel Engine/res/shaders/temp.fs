e212324b8115fabf39ddd4cd6cd8331b81cc69df
MrOutsider <dk1ofakind@gmail.com>
2021-01-31 01:35:03 -0800

    Added a struct and uniforms to controll materials.

--- Voxmel Engine/res/shaders/temp.fs
+++ Voxmel Engine/res/shaders/temp.fs
@@ -9,24 +9,31 @@ out vec4 FragColor;
 uniform vec3 objectColor;
 uniform vec3 lightColor;
 
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+}; 
+  
+uniform Material material;
+
 void main()
 {
     // Diffuse
-    float ambientStrength = 0.1f;
-    vec3 ambient = ambientStrength * vec3(1.0f);
+    vec3 ambient = vec3(1.0f) /*Light Color*/ * material.ambient;
 
     // Normal
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(LightPos - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+    float diff = max(dot(norm, lightDir), 0.0f);
+    vec3 diffuse = lightColor * (diff * material.diffuse);
 
     // Specular
-    float specularStrength = 0.5;
     vec3 viewDir = normalize(-FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor; 
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0f), material.shininess);
+    vec3 specular = lightColor * (spec * material.specular); 
 
     vec3 result = (ambient + diffuse + specular) * objectColor;
     FragColor = vec4(result, 1.0f);
--- Voxmel Engine/src/Renderer.cpp
+++ Voxmel Engine/src/Renderer.cpp
@@ -233,7 +233,7 @@ void Renderer::render()
 		//----------------------------------------------------------------------------
 		// This is just to move the spawned cubes around
 		glm::vec3 cubePositions[] = {
-			glm::vec3(5.0f,  0.0f,  0.0f),
+			glm::vec3(0.0f,  0.0f,  0.0f),
 			glm::vec3(2.0f,  5.0f, -15.0f),
 			glm::vec3(-1.5f, -2.2f, -2.5f),
 			glm::vec3(-3.8f, -2.0f, -12.3f),
@@ -246,7 +246,7 @@ void Renderer::render()
 		};
 
 		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::translate(model, cubePositions[i]);
+		model = glm::translate(model, cubePositions[i] + glm::vec3(10.0f, 0.0f, -10.0f));
 		float angle = 20.0f * i * glfwGetTime();
 		model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 		//----------------------------------------------------------------------------
@@ -303,6 +303,11 @@ void Renderer::render()
 	shaders[1].setVec3("objectColor", glm::vec3(1.0f, 0.5f, 0.31f));
 	shaders[1].setVec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
 	shaders[1].setVec3("lightPos", lightPos);
+
+	shaders[1].setVec3("material.ambient", 0.1f, 0.1f, 0.1f);
+	shaders[1].setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+	shaders[1].setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+	shaders[1].setFloat("material.shininess", 32.0f);
 
 	glBindVertexArray(tempVAO);
 	glDrawArrays(GL_TRIANGLES, 0, tempVertSize);
