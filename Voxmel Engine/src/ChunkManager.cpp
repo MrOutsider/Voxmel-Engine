3f80cdf2754f97b39cb776e1331f855046c61d86
MrOutsider <dk1ofakind@gmail.com>
2021-02-03 05:48:01 -0800

    Making basic mesher for voxels.

--- Voxmel Engine/src/ChunkManager.cpp
+++ Voxmel Engine/src/ChunkManager.cpp
@@ -10,8 +10,49 @@ ChunkManager::~ChunkManager()
 
 void ChunkManager::chunkSetterTest()
 {
+	for (unsigned int x = 0; x < chunkSize; x++)
+	{
+		if (x > ((chunkSize / 2) - 2) || x < ((chunkSize / 2) + 2))
+		{
+			for (unsigned int y = 0; y < chunkSize; y++)
+			{
+				if (y > ((chunkSize / 2) - 2) || y < ((chunkSize / 2) + 2))
+				{
+					for (unsigned int z = 0; z < chunkSize; z++)
+					{
+						if (z > ((chunkSize / 2) - 2) || z < ((chunkSize / 2) + 2))
+						{
+							chunk[z + ((y * chunkSize) + (x * chunkSize))] = 1;
+						}
+						else
+						{
+							chunk[z + ((y * chunkSize) + (x * chunkSize))] = 0;
+						}
+					}
+				}
+				else
+				{
+					for (unsigned int z = 0; z < chunkSize; z++)
+					{
+						chunk[z + ((y * chunkSize) + (x * chunkSize))] = 0;
+					}
+				}
+			}
+		}
+		else
+		{
+			for (unsigned int y = 0; y < chunkSize; y++)
+			{
+				for (unsigned int z = 0; z < chunkSize; z++)
+				{
+					chunk[z + ((y * chunkSize) + (x * chunkSize))] = 0;
+				}
+			}
+		}
+	}
 }
 
 void ChunkManager::chunkMesher()
 {
+
 }
--- Voxmel Engine/src/ChunkManager.h
+++ Voxmel Engine/src/ChunkManager.h
@@ -12,7 +12,53 @@ public:
 
 private:
 	unsigned int chunkSize = 16;
-	unsigned int chunk[4096] = {};
+	unsigned int chunk[4096];	// 16^3
+
+	float voxelSize = 0.25f;
+	float voxel[180] =
+	{ //Verts												UVs
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f,
+		(0 * voxelSize), (0 * voxelSize), (0 * voxelSize),	0.0f, 0.0f
+	};
 };
 
 #endif // !CHUNK_MANAGER_H
